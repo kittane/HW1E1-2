14cb2ab445ebe6a32c4f6c7b8e7e6a254cdae3b9
Zack <zpf5s@login.missouristate.edu>
2023-08-29 22:47:28 -0500

    Added functionality to buttons

--- app/src/main/java/com/frey/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/frey/msu/geoquiz/MainActivity.kt
@@ -2,10 +2,39 @@ package com.frey.msu.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var trueButton : Button
+    private lateinit var falseButton : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener{view: View ->
+            Toast.makeText(
+                this,
+                R.string.true_button,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        falseButton.setOnClickListener{view: View ->
+            Toast.makeText(
+                this,
+                R.string.false_button,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
