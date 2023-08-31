012a1b4d93dc89206563c5ce706870a1032e710e
Zack <zpf5s@login.missouristate.edu>
2023-08-31 00:03:17 -0500

    Converted toasts to snackbars and added fancy colors

--- app/src/main/java/com/frey/msu/geoquiz/MainActivity.kt
+++ app/src/main/java/com/frey/msu/geoquiz/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.frey.msu.geoquiz
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener{view: View ->
+/*        trueButton.setOnClickListener{view: View ->
             Toast.makeText(
                 this,
                 R.string.correct_toast,
@@ -35,6 +37,29 @@ class MainActivity : AppCompatActivity() {
                 R.string.incorrect_toast,
                 Toast.LENGTH_SHORT)
                 .show()
+        }*/
+
+        trueButton.setOnClickListener {
+            val snackBar = Snackbar.make(
+                it,
+                "Correct!",
+                Snackbar.LENGTH_LONG
+            )
+            snackBar.setTextColor(Color.BLACK)
+            snackBar.setBackgroundTint(Color.GREEN)
+            snackBar.show()
         }
+
+        falseButton.setOnClickListener{
+            val snackBar = Snackbar.make(
+                it,
+                "Incorrect!",
+                Snackbar.LENGTH_LONG
+            )
+            snackBar.setTextColor(Color.BLACK)
+            snackBar.setBackgroundTint(Color.RED)
+            snackBar.show()
+        }
+
     }
 }
